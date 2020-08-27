e1be9c6997329bb37c860f4d16709cf3c56f4a1e
ampetrov <44998320+ampetrov@users.noreply.github.com>
2020-08-27 14:34:07 +0300

    2
    Функция.

--- photo.py
+++ photo.py
@@ -2,13 +2,17 @@ import requests
 from pathlib import Path
 
 
+def download_picture(url, path, filename):
+    response = requests.get(url)
+    response.raise_for_status()
+    with open(path + filename, 'wb') as file:
+        file.write(response.content)
+
+
 Path(r"e:\Python\DVMN2\photo_inst\venv\Include\inst_photo\images").mkdir(parents=True, exist_ok=True)
 
-filename = 'satellite.jpeg'
 url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
+path = r"e:\Python\DVMN2\photo_inst\venv\Include\inst_photo\images\\"
+filename = "satellite.jpeg"
 
-response = requests.get(url)
-response.raise_for_status()
-
-with open(filename, 'wb') as file:
-    file.write(response.content)
+download_picture(url, path, filename)
