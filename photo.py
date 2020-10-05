d5f68631db5026fb56e0a7eb48e8a548a405175d
ampetrov <44998320+ampetrov@users.noreply.github.com>
2020-10-05 23:45:25 +0300

    Update photo.py
    display_links(url)

--- photo.py
+++ photo.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from pprint import pprint
 
 import requests
 
@@ -23,9 +24,21 @@ def fetch_spacex_last_launch(links, path):
         download_picture(link, path, filename="spacex{}.jpeg".format(link_number))
 
 
+def display_links(url):
+    link_list = []
+    response = requests.get(url)
+    response.raise_for_status()
+    list_info = response.json()['image_files']
+    for info in list_info:
+        link = info['file_url']
+        print("https:{}".format(link))
+
+
 Path(r"e:\Python\DVMN2\photo_inst\venv\Include\inst_photo\images").mkdir(parents=True, exist_ok=True)
 url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
 url_spacex = "https://api.spacexdata.com/v4/launches"
+url_hubblesite = "http://hubblesite.org/api/v3/image/1"
 path = r"e:\Python\DVMN2\photo_inst\venv\Include\inst_photo\images\\"
 links = get_link(url_spacex)
 fetch_spacex_last_launch(links, path)
+display_links(url_hubblesite)
