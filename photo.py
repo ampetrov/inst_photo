8f2fe66c66acdea8d880c68eca3e8c6598f14c37
ampetrov <44998320+ampetrov@users.noreply.github.com>
2020-10-05 12:32:35 +0300

    Update photo.py
    fetch_spacex_last_launch

--- photo.py
+++ photo.py
@@ -17,19 +17,15 @@ def get_link(url):
     return links
 
 
-Path(r"e:\Python\DVMN2\photo_inst\venv\Include\inst_photo\images").mkdir(parents=True, exist_ok=True)
+def fetch_spacex_last_launch(links, path):
+    for link_number, link in enumerate(links):
+        link_number += 1
+        download_picture(link, path, filename="spacex{}.jpeg".format(link_number))
 
+
+Path(r"e:\Python\DVMN2\photo_inst\venv\Include\inst_photo\images").mkdir(parents=True, exist_ok=True)
 url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
 url_spacex = "https://api.spacexdata.com/v4/launches"
 path = r"e:\Python\DVMN2\photo_inst\venv\Include\inst_photo\images\\"
-# filename = "spacex.jpeg"
-# download_picture(url, path, filename)
 links = get_link(url_spacex)
-# ['https://farm9.staticflickr.com/8617/16789019815_f99a165dc5_o.jpg',
-#  'https://farm8.staticflickr.com/7619/16763151866_35a0a4d8e1_o.jpg',
-#  'https://farm9.staticflickr.com/8569/16169086873_4d8829832e_o.png']
-
-for link_number, link in enumerate(links):
-    link_number +=1
-    download_picture(link, path, filename="spacex{}.jpeg".format(link_number))
-
+fetch_spacex_last_launch(links, path)
