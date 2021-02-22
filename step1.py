6c92fd5b2892d0dad2cdb1bc31f75e4c59921081
ampetrov <44998320+ampetrov@users.noreply.github.com>
2021-02-22 18:10:01 +0300

    upd
    +

--- step1.py
+++ step1.py
@@ -1,7 +1,6 @@
 import requests
 
 from pathlib import Path
-from pprint import pprint
 
 
 def download_picture(url, path, filename):
@@ -18,16 +17,26 @@ def get_link(url):
     return links
 
 
-# Подумай над названием функции(fetch_spacex_last_launch).
 def fetch_spacex_last_launch(links, path):
     for link_number, link in enumerate(links):
         link_number += 1
         download_picture(link, path, filename="spacex{}.jpeg".format(link_number))
 
 
-Path(r"e:\Python\DVMN2\photo_inst\venv\Include\inst_photo\\Test").mkdir(parents=True, exist_ok=True)
+def display_links(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    list_info = response.json()['image_files']
+    for info in list_info:
+        link = info['file_url']
+        print("https:{}".format(link))
+
+
+path_images = r"e:\Python\DVMN2\photo_inst\venv\Include\inst_photo\images\\"
+Path(path_images).mkdir(parents=True, exist_ok=True)
 
 url_spacex = "https://api.spacexdata.com/v4/launches/latest"
-path = r"e:\Python\DVMN2\photo_inst\venv\Include\inst_photo\Test\\"
+url_hubblesite = "http://hubblesite.org/api/v3/image/1"
 
-fetch_spacex_last_launch(get_link(url_spacex), path)
+fetch_spacex_last_launch(get_link(url_spacex), path_images)
+display_links(url_hubblesite)
